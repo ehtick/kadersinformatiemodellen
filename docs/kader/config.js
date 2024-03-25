0fd91e4729530599aa879b1f1e55665a7244dcd3
Elisabeth de Vries <elisabeth.kloren@crow.nl>
2024-03-25 14:31:35 +0100

    klaarzetten documentatie

---
+++ docs/kader/config.js
@@ -0,0 +1,32 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "DRAFT",
+  shortName: "kadersinformatiemodellen/kader",
+  specType: "technische documentatie", // vrije tekst, kleine letter
+  imprint: "digigo",
+  editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Niels Vossebeld",
+      company: "DigiGO",
+    },
+  ],
+  authors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Wouter Klein Wolterink",
+      company: "DigiGO",
+    },
+  ],
+  github: "https://github.com/nl-digigo/beheerplan",
+  license: "cc-by",
+  lint: { "no-unused-dfns": false },
+  localBiblio: {
+  BOMOS_fundament: {
+    title: "BOMOS, het fundament",
+    href: "https://gitdocumentatie.logius.nl/publicatie/bomos/fundament/",
+    status: "Actueel",
+    publisher: "Logius",
+  },
+  },
+};
